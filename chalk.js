03bad66c1db495006685a0563b018438e1dd9f21
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-04-28 23:40:52 +0530

    chalk.js

---
+++ chalk.js
@@ -0,0 +1,8 @@
+const chalk = require("chalk");
+//“chalk” — it styles text strings in the terminal.
+console.log(chalk.red("red"));
+console.log(chalk.yellow("yellow"));
+console.log(chalk.blue("blue"));
+console.log(chalk.green("green"));
+console.log(chalk.gray("gray"));
+console.log(chalk.white("white"));
