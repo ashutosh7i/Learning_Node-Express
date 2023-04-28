eb5f8a32426a69fa4832433db0784795b8376e95
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-04-28 10:38:06 +0530

    Node Modules

---
+++ modules.js
@@ -0,0 +1,21 @@
+// Modules are same as javascript libraries
+// A set of functions you want to include in your application;
+
+// Some popular and useful builtin Modules-
+// crypto.js- to handle openssl Cryptographic functions.Cryptographic
+// event -  to handle events.
+// fs - to handle file system.
+// os - info about Operating system.
+// querystring - to handle url query strings.   ( var querystring = require('querystring'); var q = querystring.parse('year=2017&month=february'); console.log(q.year);)
+// tls - to implement tls& ssl.
+
+// Popular third party Modules
+// Express - create servers easily.
+// Moment.js - easy date and time.
+// Helmet - secure your express app with http headers.
+// CORS - very important to make your api accessible and request policy error free.
+// NodeMailer - send mails form node with your own smtp server.
+// sequelize - promise based mysql,postgres,mariaDB,sqlite server.
+// Mongoose - mongodb asynchronous tool.
+// Jest - javascript testing framework.
+// chalk - console/termial string styling.
